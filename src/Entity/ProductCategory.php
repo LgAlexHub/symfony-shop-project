df188721f6bf5beb5e6794be627f47e09e39c446
AlexLg <alexlegras@hotmail.com>
2024-02-22 18:26:20 +0100

    CHANGE :     - Suppression des accesseurs des relations enfants pour éviter le eager loading

--- src/Entity/ProductCategory.php
+++ src/Entity/ProductCategory.php
@@ -2,13 +2,11 @@
 
 namespace App\Entity;
 
-use App\Entity\Trait as HelperTrait;
-use App\Repository\ProductCategoryRepository;
-
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\Common\Collections\ArrayCollection;
+
+use App\Entity\Trait as HelperTrait;
+use App\Repository\ProductCategoryRepository;
 
 #[ORM\HasLifecycleCallbacks]
 #[ORM\Entity(repositoryClass: ProductCategoryRepository::class)]
@@ -18,7 +16,7 @@ use Doctrine\Common\Collections\ArrayCollection;
  * This class represent a category of product, this class can be bind in Product Class to describe it's category.
  * This is class use Lifecycle callback throught traits.
  */
-class ProductCategory
+class ProductCategory 
 {
     use HelperTrait\TimestampableWithIdTrait;
     use HelperTrait\SluggableTrait;
@@ -26,16 +24,12 @@ class ProductCategory
     #[ORM\Column(length: 255, nullable:false, type: Types::STRING)]
     private ?string $label = null;
 
-    #[ORM\OneToMany(mappedBy: 'category', targetEntity: Product::class)]
-    private Collection $products;
-    
     /**
      * TODO : Check le lazy loading, j'ai pas envie de charger les produits à chaque fois
      * que je veux une catégorie
      */
     public function __construct()
     {
-        $this->products = new ArrayCollection();
     }
 
     /**
@@ -61,49 +55,7 @@ class ProductCategory
         return $this;
     }
 
-    /**
-     * @return Collection<int, Product>
-     */
-    public function getProducts(): Collection
-    {
-        return $this->products;
-    }
 
-    /**
-     * This method allow to set bind product to category
-     *
-     * @param Product $product
-     * @return static
-     */
-    public function addProduct(Product $product): static
-    {
-        if (!$this->products->contains($product)) {
-            $this->products->add($product);
-            $product->setCategory($this);
-        }
-
-        return $this;
-    }
-
-    /**
-     * This method allow a category to unbind a product
-     *
-     * @param Product $product
-     * @return static
-     */
-    public function removeProduct(Product $product): static
-    {
-        if ($this->products->removeElement($product)) {
-            // set the owning side to null (unless already changed)
-            if ($product->getCategory() === $this) {
-                $product->setCategory(null);
-            }
-        }
-
-        return $this;
-    }
-
-    
     /**
      * Method use by SluggableTrait to get valid source to slug
      *
