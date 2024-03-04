dde7b9bb3e871c448ce3e78d80eb25d29f61e7d6
AlexLg <alexlegras@hotmail.com>
2024-03-04 12:40:34 +0100

    FRONT CHANGE :     - correction du comportement du toast pour s'afficher uniquement s'il y a un feedback à afficher     - ajout de la persistance des données déclenchés par vue dans la base de données, avec un retour feedback positif s'il y en a un         * TODO : implémenter le feedback en cas d'erreur     -

--- assets/vue/controllers/components/Toast.vue
+++ assets/vue/controllers/components/Toast.vue
@@ -1,66 +1,64 @@
 <template>
-    <div :class="toastClass">
-        <p  v-if="message !== null && message !== ''" class="text-center text-neutral-50">
+    <div v-if="message !== null && message !== ''" :class="toastClass">
+        <p class="text-center text-neutral-50">
             {{ message }}
         </p>
-        <button @click="toggle">test</button>
     </div>
 </template>
 
 <script>
 export default {
-    name : "toast",
-    props : {
-        propsMessage : {
-            type : String,
-            required : true,
+    name: "toast",
+    props: {
+        propsMessage: {
+            type: String,
+            required: true,
         },
-        propsType : {
-            type : String, 
-            required : false
+        propsType: {
+            type: String,
+            required: false
         }
-    }, 
+    },
     data() {
         return {
-            message : "gzezfze",
-            displayType : "success",
-            durationInSecond : 5,
-            visibility : false,
-            types : {
-                'success' : ['border-lime-100', 'from-lime-800', 'to-green-500'],
-                'danger'  : ['border-red-100', 'from-red-800', 'to-amber-400'],
-                'default' : ['border-sky-400', 'bg-blue-300', 'bg-opacity-70']
+            message: null,
+            displayType: null,
+            durationInSecond: 5,
+            visibility: false,
+            types: {
+                'success': ['border-lime-100', 'from-lime-800', 'to-green-500'],
+                'danger': ['border-red-100', 'from-red-800', 'to-amber-400'],
+                'default': ['border-sky-400', 'bg-blue-300', 'bg-opacity-70']
             },
-            defaultClasses : ['z-4', 'ease-in', 'duration-700', 'transition-opacity', 'absolute', 'bottom-0', 'left-1/2', 'transform', '-translate-x-1/2', 'w-32', 'border-2', 'rounded', 'px-2', 'py-1', 'mb-2']
+            defaultClasses: ['z-4', 'ease-in', 'duration-700', 'transition-opacity', 'absolute', 'bottom-0', 'left-1/2', 'transform', '-translate-x-1/2', 'w-32', 'border-2', 'rounded', 'px-2', 'py-1', 'mb-2']
         };
     },
     computed: {
-            toastClass() {
-                let cssClasses = this.defaultClasses;
-                if(this.type !== null && this.types.hasOwnProperty(this.displayType)){
-                    cssClasses.push('bg-gradient-to-t', ...this.types[this.displayType])
-                }else{
-                    cssClasses.push(...this.types[this.displayType])
-                }
-                // cssClasses.push(
-                //     ... this.displayType !== null && this.types.hasOwnProperty(this.displayType)
-                //         ? [].push('bg-gradient-to-t', ...this.types[this.displayType])
-                //         : this.types[this.displayType]
-                // )
-                cssClasses.push(`opacity-${this.visibility ? '100' : '0'}`)
-                return cssClasses.join(' ');
-            },
+        toastClass() {
+            let cssClasses = this.defaultClasses;
+            if (this.type !== null && this.types.hasOwnProperty(this.displayType)) {
+                cssClasses.push('bg-gradient-to-t', ...this.types[this.displayType])
+            } else {
+                cssClasses.push(...this.types[this.displayType])
+            }
+            // cssClasses.push(
+            //     ... this.displayType !== null && this.types.hasOwnProperty(this.displayType)
+            //         ? [].push('bg-gradient-to-t', ...this.types[this.displayType])
+            //         : this.types[this.displayType]
+            // )
+            cssClasses.push(`opacity-${this.visibility ? '100' : '0'}`)
+            return cssClasses.join(' ');
+        },
     },
     methods: {
-        toggle(){
+        toggle() {
             this.visibility = !this.visibility;
             setTimeout(() => this.visibility = !this.visibility, this.durationInSecond * 1000);
         }
     },
-    mounted(){
-        if(this.propsMessage){
-
-        }
-    }
+    mounted() {
+        this.displayType = this.propsType;
+        this.message = this.propsMessage;
+    },
 };
 </script>
--- assets/vue/controllers/orders/productsForm.vue
+++ assets/vue/controllers/orders/productsForm.vue
@@ -1,12 +1,11 @@
 <template>
-  <toast v-if="false"></toast>
+  <toast v-if="feedbackMessage !== null && feedbackType !== null" :propsMessage="feedbackMessage"
+    :propsType="feedbackType"></toast>
   <div class="grid grid-cols-5 gap-5 my-2">
     <div class="px-5 col-span-2 h-100 overflow-scroll">
       <p class="text-xl">Votre panier</p>
       <template v-for="orderItem in orderItems" :key="orderItem.item.slug">
-        <div
-          class="border-2 my-1 border-solid border-lime-950 rounded px-2 py-1 grid grid-cols-3"
-        >
+        <div class="border-2 my-1 border-solid border-lime-950 rounded px-2 py-1 grid grid-cols-3">
           <div class="grid grid-rows-2 col-span-2">
             <p class="text-md">
               {{ orderItem.item.product.name }} - {{ orderItem.item.weight }}
@@ -15,20 +14,13 @@
             <div class="grid grid-cols-3">
               <p class="">{{ (orderItem.item.price / 100).toFixed(2) }} €</p>
               <div class="inline col-span-2 place-self-center py-1">
-                <button
-                  class="px-2 border-2 border-solid border-lime-950 rounded"
-                  @click="editQuantity(orderItem.item.slug)"
-                >
+                <button class="px-2 border-2 border-solid border-lime-950 rounded"
+                  @click="editQuantity(orderItem.item.slug)">
                   <span class="">-</span>
                 </button>
-                <span
-                  class="px-2 mx-1 border-2 border-solid border-lime-950 rounded"
-                  >{{ orderItem.quantity }}</span
-                >
-                <button
-                  class="px-2 border-2 border-solid border-lime-950 rounded"
-                  @click="editQuantity(orderItem.item.slug, true)"
-                >
+                <span class="px-2 mx-1 border-2 border-solid border-lime-950 rounded">{{ orderItem.quantity }}</span>
+                <button class="px-2 border-2 border-solid border-lime-950 rounded"
+                  @click="editQuantity(orderItem.item.slug, true)">
                   +
                 </button>
               </div>
@@ -37,8 +29,7 @@
           <div>
             <button
               class="border-2 bg-red-600 text-white hover:bg-red-900 text-xs border-solid border-lime-950 px-2 py-1 w-full h-1/2 my-5"
-              @click="removeItem(orderItem.item.slug)"
-            >
+              @click="removeItem(orderItem.item.slug)">
               Suppr.
             </button>
           </div>
@@ -46,9 +37,7 @@
       </template>
     </div>
     <div class="col-span-3 px-2">
-      <div
-        class="border-2 border-solid border-lime-950 px-2 rounded-sm py-1 w-full"
-      >
+      <div class="border-2 border-solid border-lime-950 px-2 rounded-sm py-1 w-full">
         <products-fetcher @product-added="onProductAdded"></products-fetcher>
       </div>
     </div>
@@ -61,87 +50,120 @@ import ProductsFetcher from "./ProductsFetcher.vue";
 import toast from "../components/Toast.vue";
 
 export default {
-    name : 'products-form',
-    components : {
-      'products-fetcher' : ProductsFetcher,
-      toast : toast
-    }, 
-    props: {
-        orderUuid: {
-            type: String,
-            required: true,
-        },
-        prefetchOrderItem: {
-            type: String,
-            required: true,
-        },
+  name: 'products-form',
+  components: {
+    'products-fetcher': ProductsFetcher,
+    'toast': toast
+  },
+  props: {
+    orderUuid: {
+      type: String,
+      required: true,
     },
-    data() {
-        return {
-            orderItems: [],
-            productQueryString: "",
-            debounceTimeout: null,
-            animationbool: false,
-        };
+    prefetchOrderItem: {
+      type: String,
+      required: true,
     },
-    computed: {
-        
+  },
+  data() {
+    return {
+      orderItems: [],
+      productQueryString: "",
+      debounceTimeout: null,
+      feedbackMessage: null,
+      feedbackType: null,
+    };
+  },
+  methods: {
+    /**
+     * action type bool , true for addition , false sub
+     */
+    editQuantity(slug, actionType) {
+      let itemIndex = this.findItemIndexBySlug(slug);
+      let targetedProduct = this.orderItems[itemIndex];
+
+      if (itemIndex === -1) {
+        this.feedbackMessage = "Erreur : produit dans le panier introuvable !"
+        this.feedbackType = "danger";
+        return;
+      }
+
+      // addition
+      if(actionType){
+        this.orderItems[itemIndex].quantity += 1;
+        this.persistChangeInDatabase('addition', targetedProduct.item.slug);
+      }
+
+      // soustraction
+      if(!actionType){
+        if (this.orderItems[itemIndex].quantity < 2){
+          this.orderItems.splice(itemIndex, 1);
+        }else{
+          this.orderItems[itemIndex].quantity -= 1
+        }
+        this.persistChangeInDatabase('substraction', targetedProduct.item.slug);
+      }
     },
-    methods: {
-        editQuantity(slug, isAddition) {
-            let itemIndex = this.orderItemBySlug(slug);
-            if (itemIndex === -1) {
-                // throw error, pop up , feedback client
-            }
-            if (!isAddition) {
-                this.orderItems[itemIndex].quantity =
-                    this.orderItems[itemIndex].quantity > 0
-                        ? this.orderItems[itemIndex].quantity - 1
-                        : 0;
-            }
-            else {
-                this.orderItems[itemIndex].quantity += 1;
-            }
-        },
-        removeItem(slug) {
-            let itemIndex = this.orderItemBySlug(slug);
-            if (itemIndex !== -1) {
-                this.orderItems.splice(itemIndex, 1);
-            }
-        },
-        orderItemBySlug(slug) {
-            return this.orderItems.findIndex((element) => element.item.slug === slug);
-        },
-        onProductAdded(payloadItem) {
-            let itemIndex = this.orderItemBySlug(payloadItem.slug);
-            if (itemIndex === -1) {
-                itemIndex = this.orderItems.length;
-                this.orderItems[itemIndex] = {
-                    quantity: 0,
-                    item: {
-                        price: payloadItem.price,
-                        product: {
-                            name: payloadItem.product.name,
-                            slug: payloadItem.product.slug,
-                        },
-                        slug: payloadItem.slug,
-                        weight: payloadItem.weight,
-                        weightType: payloadItem.weightType,
-                    },
-                };
-            }
-            this.orderItems[itemIndex].quantity += 1;
-            var bodyFormData = new FormData();
-            bodyFormData.append("form[productReferenceSlug]", this.orderItems[itemIndex].item.slug);
-            axios
-                .post(`/api/commandes/${this.orderUuid}/ajouter-produit`, bodyFormData)
-                .then((res) => console.log(res))
-                .catch((err) => console.error(err));
-        },
+    findItemIndexBySlug(slug) {
+      return this.orderItems.findIndex((element) => element.item.slug === slug);
     },
-    mounted() {
-        this.orderItems = JSON.parse(this.prefetchOrderItem);
-        // vm.$forceUpdate();
+    onProductAdded(payloadItem) {
+      let itemIndex = this.findItemIndexBySlug(payloadItem.slug);
+      if (itemIndex === -1) {
+        itemIndex = this.orderItems.length;
+        this.orderItems[itemIndex] = {
+          quantity: 1,
+          item: {
+            price: payloadItem.price,
+            product: {
+              name: payloadItem.product.name,
+              slug: payloadItem.product.slug,
+            },
+            slug: payloadItem.slug,
+            weight: payloadItem.weight,
+            weightType: payloadItem.weightType,
+          },
+        }
+        this.persistChangeInDatabase('addition', payloadItem.slug);
+      }
     },
+    persistChangeInDatabase(action, slug) {
+      let apiEndPoint = null;
+      switch (action) {
+        case 'addition':
+          apiEndPoint = 'ajouter-produit'
+        break;
+        case 'substraction':
+          apiEndPoint = 'supprimer-produit'
+        break;
+      }
+
+      if(slug === null || apiEndPoint === null){
+        // ajouter un feedback et dire pourquoi on envoie rien au serveur
+        return ;
+      }
+      let bodyFormData = new FormData();
+      bodyFormData.append("form[productReferenceSlug]", slug);
+      axios
+        .post(`/api/commandes/${this.orderUuid}/${apiEndPoint}`, bodyFormData)
+        .then((res) => {
+          this.setToastMessage('Panier sauvegardé avec succès !', 'success')
+        })
+        .catch((err) => console.error(err));
+    },
+    setToastMessage(message, type){
+      this.feedbackMessage = message; 
+      this.feedbackType = type;
+      setTimeout(() => {
+        this.feedbackMessage = null;
+        this.feedbackType = null;
+      } , 3 * 1000);
+    }
+  },
+
+  mounted() {
+    this.orderItems = JSON.parse(this.prefetchOrderItem);
+    // vm.$forceUpdate();
+  },
 };
 </script>
