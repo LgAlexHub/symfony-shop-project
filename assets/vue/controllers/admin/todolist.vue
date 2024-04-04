dbb3796b72ac642f47706ae21ad865c45cfeed04
AlexLg <alexlegras@hotmail.com>
2024-04-04 16:50:17 +0200

    Refont todo list

--- assets/vue/controllers/admin/todolist.vue
+++ assets/vue/controllers/admin/todolist.vue
@@ -1,73 +1,51 @@
 <template>
     <div>
-        <h1 @click="isVisible = !isVisible" class="text-center my-3 text-2xl font-san underline text-slate-800">Todo list<i class="ml-3 fa-solid fa-circle-down"></i></h1>
-        <div :class="{ 'hidden': !isVisible, 'bg-gray-200': isVisible }" class="px-2 py-1 mx-2 my-4 border-2 border-red-700 rounded">
-            
+        <h1 @click="isVisible = !isVisible" class="cursor-pointer text-center my-3 text-2xl text-gray-800 hover:text-blue-600">
+            Todo List
+            <i :class="{'rotate-180': isVisible}" class="ml-2 fas fa-chevron-down"></i>
+        </h1>
+        <div :class="{ 'hidden': !isVisible, 'bg-gray-100': isVisible }" class="px-4 py-2 mx-4 my-6 border border-gray-300 rounded-lg shadow-md">
             <ul class="list-none">
-                <li>
-                    <i class="fa-solid fa-check"></i>
-                    <strong class="line-through">- Finir la création de commande</strong>
-                </li>
-                <li class="line-through font-bold">
-                    <i class="fa-solid fa-check"></i>
-                    Gérer les commandes le back-office
-                    <ul class="[&>li]:indent-12">
-                        <li>* Afficher les produits de la commandes</li>
-                        <li>* Afficher si la commandes est complète (possède des produits)</li>
-                        <li>* Ajouter une addresse de livraison dans la commande</li>
-                        <li>* Afficher les infos d'adresse dans le back-office</li>
-                        <li>* Ajouter une variable pour dire si l'owner du site à déjà gérer la commande</li>
-                        <li>* Ajouter dans orders.index le total du panier pour chaque commande</li>
-                        <li>* Ajouter le prix du panier total dans orders.show</li>
+                <li class="text-gray-600">
+                    <i class="fas fa-hourglass-start mr-2 text-gray-400"></i>
+                    Gérer les commandes du [back-office] :
+                    <ul class="[&>*]:ml-10 list-disc">
+                        <li class="text-gray-500">Envoyer un mail au client lorsque la commande est marquée comme validée (ajouter un système de vérification pour empêcher le spam de mail).</li>
+                        <li class="text-gray-500">Ajouter des animations de chargement pour le composant Vue admin/order/table lorsqu'il fetch des données en db.</li>
                     </ul>
                 </li>
-                <li class="text-black font-bold line-through">
-                    <i class="fa-solid fa-check"></i>
-                    Affichage front 
-                    <ul class="[&>*]:indent-12">
-                        <li>* Paginer les résultats pour alléger les requêtes de la bdd</li>
-                        <li>* Ajouter des filtres par catégories</li>
+                <li class="text-gray-600">
+                    <i class="fas fa-hourglass-start mr-2 text-gray-400"></i>
+                    Back-Office :
+                    <ul class="[&>*]:ml-10 list-disc">
+                        <li class="text-gray-500">Ajouter un endroit dans le BO où on peut ajouter des produits phares.</li>
+                        <li class="text-gray-500 font-bold">Faire une refonte de l'affichage des produits en utilisant un composant Vue (cf : admin/order/table.vue).</li>
+                        <li class="text-gray-500">Ajouter la fonctionnalité d'upload d'image pour les productReferences.</li>
                     </ul>
                 </li>
-                <li class="text-slate-600">
-                    <i class="fa-solid fa-hourglass-start"></i>
-                    Améliorer l'ui/ux de la prise de commande commande
-                    <ul class="[&>*]:indent-12">
-                        <li>* Afficher une liste de produit au chargement de la page , le tout paginer</li>
-                        <li>* Rendre la page responsive</li>
-                        <li>* Améliorer le desgin</li>
+                <li class="text-gray-600">
+                    <i class="fas fa-hourglass-start mr-2 text-gray-400"></i>
+                    Faire une refonte complète du système de prise de commande [côté client] :
+                    <ul class="[&>*]:ml-10 list-disc">
+                        <li class="text-gray-500">Composant Vue dynamique.</li>
+                        <li class="text-gray-500">Page responsive.</li>
+                        <li class="text-gray-500">Beau design.</li>
+                        <li class="text-gray-500">Montrer les étapes pour passer une commande.</li>
+                        <li class="text-gray-500">Implémenter vue-store pour la prise de commande (à voir).</li>
                     </ul>
                 </li>
-                <li class="text-slate-600">
-                    <i class="fa-solid fa-hourglass-start"></i>
-                    Back-Office
-                    <ul class="[&>*]:indent-12">
-                        <li>* Ajouter un endroit dans le bo pour ajouter des produits phares pour l'instant limité à 3 :
-                            <ul class="[&>*]:indent-24">
-                                <li>-- Ajouter un bouton ajouter au produit phare</li>
-                                <li>-- Amélioration : Ajouter un carrousel</li>
-                            </ul>
-                        </li>
-                        <li>* Ajouter des filtres dans le BO pour produits, commandes </li>
-                        <li>* Ajouter la fonctionnalité d'upload d'image pour les productReferences</li>
+                <li class="text-blue-600">
+                    <i class="fas fa-arrow-up mr-2 text-blue-400"></i> Amélioration à faire plus tard :
+                    <ul class="[&>*]:ml-10 list-disc">
+                        <li class="text-blue-500">Ajouter une page récap de commande , possibilité de générer un pdf.</li>
+                        <li class="text-blue-500">Ajouter une propriété couleur à ProductCategory.</li>
                     </ul>
                 </li>
-                <li class="text-sky-600">
-                    <i class="fa-solid fa-arrow-up"></i> Amélioration à faire plus tard
-                    <ul class="[&>*]:indent-12">
-                        <li class="line-through">* Ajouter une fixture pour Order et OrderProductRef</li>
-                        <li>* Supprimer order.show.twig</li>
-                        <li>* Ne pu actualiser la bdd à chaque clique côté client lors de la sélection product</li>
-                        <li>* Ajouter une page récap de commande , possibilité de générer un pdf</li>
-                        <li>* Montrer les étapes pour passer une commande</li>
-                        
-                    </ul>
-                </li>
-                <div class="text-lime-600">
-                    <i class="fa-solid fa-arrow-up"></i> Idée à planifier
-                    <ul class="[&>*]:indent-12">
-                        <li>Implémenter vue-store pour la prise de commande</li>
-                        <li>Auto delete des commandes non valides (avec validation des produits)</li>
+                <div class="text-green-600">
+                    <i class="fas fa-arrow-up mr-2 text-green-400"></i> Idée à planifier :
+                    <ul class="[&>*]:ml-10 list-disc">
+                        <li class="text-green-500">Trouver un moyen d'isoler les composant vue admin et client, pour que l'utilisateur ne charge pas des composants qu'il n'utilisera jamais !</li>
+                        <li class="text-green-500">Ajouter une propriété pour savoir si le client a bien payé le site owner.</li>
                     </ul>
                 </div>
             </ul>
